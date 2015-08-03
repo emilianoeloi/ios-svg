8882bad2832c76a85319f936d728cf4006871296
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2015-08-03 16:17:40 -0300

    Inclusão de um projeto XCode

---
+++ iossvg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iossvg
+//
+//  Created by Emiliano Barbosa on 8/3/15.
+//  Copyright (c) 2015 Bocamuchas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iossvg/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iossvg
+//
+//  Created by Emiliano Barbosa on 8/3/15.
+//  Copyright (c) 2015 Bocamuchas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
